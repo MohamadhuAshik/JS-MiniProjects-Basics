76fcfd0a514dd89166940e924db66c0c6da35a8b
MohamadhuAshik <mohamadashik268@gmail.com>
2023-10-11 17:15:48 +0530

    Add Commend Line

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+ // Celcious To Farenheat Converter
  function converter() {
      let celcious = Number(document.getElementById("celsius").value);
      let farenheat = (celcious * (9 / 5) + 32);
      let result = document.getElementById("Fahrenheit");
      result.innerHTML = "Farenheat Value Is:"+farenheat+"F";
  }
-
+// Letter Counter
 function counter(){
 let letter = document.getElementById("Letter").value;
 let count = letter.length;
 let click = document.getElementById("LetterCount");
   click.innerHTML="Your Letter Count Is:"+count;
 }
-
+// Vote Machine
 function age(){
     let age = Number(document.getElementById("age").value)
     let Vote = age>=18 ? "Eligible For Vote" :  "Not Eligible For Vote";
